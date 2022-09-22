84a9e6bffd8593534bd41b3c9d0990145dc56794
김영욱 <youngwook.kim@mobin-inc.com>
2022-09-22 10:57:07 +0900

    env 설정

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
-import eslint from 'vite-plugin-eslint'
+import eslint from "vite-plugin-eslint";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 
-export default defineConfig({
-  plugins: [eslint(), createVuePlugin()],
-  server: {
-    port: 3000
-  },
-  preview: {
-    port: 8080
-  }
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "")
+    return {
+        plugins: [eslint(), createVuePlugin()],
+        server: {
+            port: env.VITE_APP_PORT,
+            host: env.VITE_APP_HOST
+        },
+        preview: {
+            port: 8080
+        }
+    };
+});
